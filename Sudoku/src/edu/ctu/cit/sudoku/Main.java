2d462bd911090c0f4a78ed2f9a3e6184804ecd3d
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-08-14 14:43:58 +0700

    add MainWindow class and invoke it from function main

--- Sudoku/src/edu/ctu/cit/sudoku/Main.java
+++ Sudoku/src/edu/ctu/cit/sudoku/Main.java
@@ -5,6 +5,8 @@
  */
 package edu.ctu.cit.sudoku;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  *
  * @author charlie
@@ -14,8 +16,13 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws InterruptedException, InvocationTargetException {
+        java.awt.EventQueue.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
     }
     
 }
