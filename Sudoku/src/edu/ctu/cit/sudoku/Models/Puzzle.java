26cee5b5eff6e5e096798b30c8cb97940cb78a29
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-08-14 19:20:47 +0700

    add int[][] board, const BOARD_SIZE and neccessary methods

--- Sudoku/src/edu/ctu/cit/sudoku/Models/Puzzle.java
+++ Sudoku/src/edu/ctu/cit/sudoku/Models/Puzzle.java
@@ -10,5 +10,29 @@ package edu.ctu.cit.sudoku.Models;
  * @author charlie
  */
 public class Puzzle {
+    public static final int BOARD_SIZE = 9;
+            
+    private final int[][] board;
     
+    public Puzzle() {
+        this.board = new int[BOARD_SIZE][BOARD_SIZE];
+    }
+    
+    private boolean isValidCoordinate(int x, int y) {
+        return (x >= 0) && (y >= 0) && (x < BOARD_SIZE) && (y < BOARD_SIZE);
+    }
+    
+    public int get(int x, int y) {
+        if (!isValidCoordinate(x, y)) {
+            throw new ArrayIndexOutOfBoundsException("Board coordinate must be between 0 to 8");
+        }
+        return board[x][y];
+    }
+    
+    public void set(int x, int y, int value) {
+        if (!isValidCoordinate(x, y)) {
+            throw new ArrayIndexOutOfBoundsException("Board coordinate must be between 0 to 8");
+        }
+        board[x][y] = value;
+    }
 }
