f92391304b21304dcf64940d798fdaf924b1dbe4
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-09-27 20:16:40 +0700

    add class HighScoreDbHelper

--- Sudoku/src/edu/ctu/cit/sudoku/Databases/HighScoreSchema.java
+++ Sudoku/src/edu/ctu/cit/sudoku/Databases/HighScoreSchema.java
@@ -9,14 +9,19 @@ package edu.ctu.cit.sudoku.Databases;
  *
  * @author charlie
  */
-public class Tables {
-    public static class HighScore {
-        public static final String DATABASE_NAME = "highscore.db";
-        
+public class HighScoreSchema {
+
+    public static final String DATABASE_NAME = "highscore.db";
+
+    public static class HighScoreTable {
+
+        public static final String TABLE_NAME = "highscore";
+
         public static class Cols {
+
             public static final String ID = "id";
             public static final String USER_NAME = "name";
-            public static final String SOCRE = "score";
+            public static final String SCORE = "score";
         }
     }
 }
