60ce64ce4ed9fca264bce2e65f71226e10b4271c
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-09-28 07:49:51 +0700

    add createTable(), insert(), and selectAll() to HighScoreDbHelper

--- Sudoku/src/edu/ctu/cit/sudoku/Databases/HighScoreDbHelper.java
+++ Sudoku/src/edu/ctu/cit/sudoku/Databases/HighScoreDbHelper.java
@@ -7,20 +7,25 @@ package edu.ctu.cit.sudoku.Databases;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
  * @author charlie
  */
 public class HighScoreDbHelper implements AutoCloseable {
-    private static final String URL = "jdbc:sqlite:" + HighScoreSchema.DATABASE_NAME;
-    
-    private Connection conn = null;    
 
-    public HighScoreDbHelper() {
+    private static final String URL = String.format("jdbc:sqlite:%s", HighScoreSchema.DATABASE_NAME);
+
+    private Connection conn = null;
+
+    public HighScoreDbHelper() throws SQLException {
     }
-    
+
     public void open() throws SQLException {
         this.close();
         this.conn = DriverManager.getConnection(HighScoreDbHelper.URL);
@@ -33,4 +38,66 @@ public class HighScoreDbHelper implements AutoCloseable {
             this.conn = null;
         }
     }
+
+    public void createTable() throws SQLException {
+        // SQL statement for creating a new table
+        String sql = String.format(
+                "CREATE TABLE IF NOT EXISTS %s (\n"
+                + "	%s integer PRIMARY KEY,\n"
+                + "	%s text NOT NULL,\n"
+                + "	%s integer NOT NULL\n"
+                + ");",
+                HighScoreSchema.HighScoreTable.TABLE_NAME,
+                HighScoreSchema.HighScoreTable.Cols.ID,
+                HighScoreSchema.HighScoreTable.Cols.USER_NAME,
+                HighScoreSchema.HighScoreTable.Cols.SCORE
+        );
+
+        try (Statement stmt = this.conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        }
+    }
+
+    public void insert(String name, int score) throws SQLException {
+        String sql = String.format(
+                "INSERT INTO %s(%s, %s) VALUES(?,?)",
+                HighScoreSchema.HighScoreTable.TABLE_NAME,
+                HighScoreSchema.HighScoreTable.Cols.USER_NAME,
+                HighScoreSchema.HighScoreTable.Cols.SCORE
+        );
+
+        try (PreparedStatement pstmt = this.conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, score);
+            pstmt.executeUpdate();
+        }
+    }
+
+    public ArrayList<Object[]> selectAll() throws SQLException {
+        String sql = String.format(
+                "SELECT %s, %s, %s FROM %s ORDER BY %s ASC",
+                HighScoreSchema.HighScoreTable.Cols.ID,
+                HighScoreSchema.HighScoreTable.Cols.USER_NAME,
+                HighScoreSchema.HighScoreTable.Cols.SCORE,
+                HighScoreSchema.HighScoreTable.TABLE_NAME,
+                HighScoreSchema.HighScoreTable.Cols.SCORE
+        );
+        ArrayList<Object[]> result = new ArrayList<>();
+
+        try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+            // loop through the result set
+            while (rs.next()) {
+                result.add(new Object[]{
+                    rs.getInt(1),
+                    rs.getString(2),
+                    rs.getInt(3)
+                });
+            }
+        }
+
+        return result;
+    }
+
 }
